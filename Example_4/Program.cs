17f27642803b5d75a2c50de9c8905aee75c67bd0
zhoma96 <zhchyrmashuulu@gmail.com>
2024-05-23 23:15:30 +0300

    Added Example 4 with If and Else

---
+++ Example_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Please, Enter Your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha") // ToLower - Brings all to lower case.
+{
+    Console.WriteLine("Woow, this Masha. Welcome");
+}
+else
+{
+    Console.Write("Hi, " + username);
+}
