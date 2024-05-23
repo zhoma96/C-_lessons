df8765ddebcd1fa0e9d70de0056933f379002c0d
zhoma96 <zhchyrmashuulu@gmail.com>
2024-05-23 23:26:37 +0300

    Added Example 5

---
+++ Example_5/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 2;
+int c = 4;
+int d = 6;
+int e = 3;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("Maximum is " + max);
